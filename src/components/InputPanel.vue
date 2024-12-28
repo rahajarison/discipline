7408f310746daa27b4e9657c0b9ba37e967df90c
chunk <rahajarison.johnny@gmail.com>
2024-12-29 00:52:49 +0100

    Add undo and redo feature and fix id of actions

--- src/components/InputPanel.vue
+++ src/components/InputPanel.vue
@@ -1,34 +1,37 @@
 <template>
     <div class="w-1-3">
-        <h1 class="">Action Input</h1>
+        <h1>Action Input</h1>
         <ul>
             <li><span>A:</span> Jump In</li>
             <li><span>Z:</span> Dash In</li>
             <li><span>Enter:</span> Gain Round</li>
         </ul>
         <div class="controls">
-            <button @click="undo">Undo</button>
-            <button @click="redo">Redo</button>
+            <button @click="undo" class="btn-primary">Undo</button>
+            <button @click="redo" class="btn-primary">Redo</button>
         </div>
         <div class="active-player">
-            <p>Active Player: {{ state.activePlayer }}</p>
-            <button @click="togglePlayer"
-                class="btn-primary">Switch
-                Player</button>
+            <p>Active Player: {{ activePlayer }}</p>
+            <button  @click="togglePlayer" class="btn-primary">
+                Switch Player
+            </button>
         </div>
     </div>
 </template>
 
 <script>
+import { computed } from 'vue';
 import { useGameStore } from '../store/gameStore';
 
 export default {
     setup() {
         const gameStore = useGameStore();
 
+        const activePlayer = computed(() => gameStore.activePlayer)
+
         return {
             state: gameStore,
-            activePlayer: gameStore.activePlayer,
+            activePlayer,
             togglePlayer: gameStore.togglePlayer,
             undo: gameStore.undo,
             redo: gameStore.redo,
--- src/store/gameStore.js
+++ src/store/gameStore.js
@@ -4,12 +4,15 @@ export const useGameStore = defineStore('game', {
     state: () => ({
         rounds: [{ actions: [
             {
+                "id": 'unique-id',
                 "type": "Jump in",
                 "player": "P1",
                 "timestamp": "99"
              }
         ] }],
         activePlayer: 'P1',
+        undoStack: [],
+        redoStack: [],
     }),
     actions: {
         togglePlayer() {
@@ -17,10 +20,20 @@ export const useGameStore = defineStore('game', {
             console.log("Change player" + this.activePlayer);
         },
         undo() {
-            console.log('Undo action');
+            const currentRound = this.rounds[this.rounds.length - 1];
+            if (currentRound.actions.length > 0) {
+                const lastAction = currentRound.actions.pop();
+                this.undoStack.push(lastAction);
+                console.log('Undo:', lastAction);
+            }
         },
         redo() {
-            console.log('Redo action');
+            if (this.undoStack.length > 0) {
+                const lastUndo = this.undoStack.pop();
+                this.redoStack.push(lastUndo);
+                this.rounds[this.rounds.length - 1].actions.push(lastUndo);
+                console.log('Redo:', lastUndo);
+            }
         },
     },
 });
